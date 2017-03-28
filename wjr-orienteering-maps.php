fdd54024c48c4f02fd2ee71d82574c281d0583c8
xc-racer99 <xc-racer2@live.ca>
2017-03-28 09:45:19 -0700

    Cleanup js loading
    Only load the leaflet js files when necessary, don't depend on the plugin being located in a folder called wjr-orienteering-maps

--- wjr-orienteering-maps.php
+++ wjr-orienteering-maps.php
@@ -12,9 +12,8 @@ function shortcodes_init()
 	{
  		// do something to $content, if needed
 
-
-		// Enqueue the script in the footer
-		wp_enqueue_script('wjr-map-script', plugins_url() . '/wjr-orienteering-maps/mapsMap.js', null, null, true);
+		// Enqueue the script in the footer, with leaflet dependencies
+		wp_enqueue_script('wjr-map-script', plugins_url('mapsMap.js', __FILE__), array('leaflet-base-js', 'leaflet-markercluster-script'), null, true);
 
 		// always return
 		return '<div id="mapid" style="height: 450px;"></div>';
@@ -28,14 +27,15 @@ add_action('init', 'shortcodes_init');
 function wjr_enqueue_resources()
 {
 		// Leaflet base
-		wp_enqueue_script('leaflet-base', '//unpkg.com/leaflet@1.0.3/dist/leaflet.js');
-		wp_enqueue_style('leaflet-markercluster-css', '//unpkg.com/leaflet@1.0.3/dist/leaflet.css');
+		wp_register_script('leaflet-base-js', '//unpkg.com/leaflet@1.0.3/dist/leaflet.js', null, true);
+		wp_enqueue_style('leaflet-base-style', '//unpkg.com/leaflet@1.0.3/dist/leaflet.css');
 
 		// Leaflet markercluster
-		wp_enqueue_script('leaflet-markercluster-script', plugins_url() . '/wjr-orienteering-maps/leaflet.markercluster.js');
-		wp_enqueue_style('leaflet-markercluster-base-css', plugins_url() . '/wjr-orienteering-maps/MarkerCluster.Default.css');
-		wp_enqueue_style('leaflet-markercluster-css', plugins_url() . '/wjr-orienteering-maps/MarkerCluster.css');
+		wp_register_script('leaflet-markercluster-script', plugins_url('leaflet.markercluster.js', __FILE__), null, true);
+		wp_enqueue_style('leaflet-markercluster-base-css', plugins_url('MarkerCluster.Default.css', __FILE__));
+		wp_enqueue_style('leaflet-markercluster-css', plugins_url('MarkerCluster.css', __FILE__));
 
-		wp_enqueue_style('leaflet-markercluster-css', plugins_url() . '/wjr-orienteering-maps/mapsPopup.css');
+		// Our custom popup CSS
+		wp_enqueue_style('wjr-popup-css', plugins_url('mapsPopup.css', __FILE__));
 }
 add_action('wp_enqueue_scripts', 'wjr_enqueue_resources');
